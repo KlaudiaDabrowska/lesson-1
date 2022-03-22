9b6014e8afb07c05e49975057d84d18986bff207
KlaudiaDabrowska <klaudia@DESKTOP-U5H299B.localdomain>
2022-03-22 10:23:30 +0100

    Create class TurboNumber. Unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,20 @@
-import { number } from "./index";
+import {TurboNumber} from "./index"
 
-describe("number", function () {
-  it("works", () => {
-    expect(number).toBe(5);
+describe("math operation", function () {
+  it("substract", () => {
+    const test = new TurboNumber(10);
+    test.substract(5)
+    expect(test.result()).toBe(5);
   });
+  it("divide", () => {
+    const test = new TurboNumber(12);
+    test.divide(2);
+    expect(test.result()).toBe(6)
+  })
+  it("doesn`t divide by 0", () => {
+    const test = new TurboNumber(4);
+    test.divide(0);
+    expect(test.result()).toThrowError;
+    // expect( () => test.divide(0)).toThrow("can`t divide by zero")
+  } )
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export const number = 5;
+export class TurboNumber {
+    num: number; 
+    constructor( num: number){
+        this.num = num
+    }
+
+    substract(x: number): void {
+       this.num = this.num - x;
+    //    if ( x === Number.MAX_SAFE_INTEGER && this.num < 0){
+    //        throw new Error("integer overflow");
+    //    }
+    }
+
+    divide(x:number): void {
+        this.num = this.num / x
+    }
+
+    result(): number{
+        return this.num
+    }
+}
