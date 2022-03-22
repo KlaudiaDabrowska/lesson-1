64231878fc8f70c11cae0f3c96113800cf3a74cc
KlaudiaDabrowska <klaudia@DESKTOP-U5H299B.localdomain>
2022-03-22 11:08:05 +0100

    Use beforeEach()

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,18 +1,19 @@
 import {TurboNumber} from "./index"
 
+let test: TurboNumber;
+
 describe("math operation", function () {
+  beforeEach(()=> { test = new TurboNumber(10) })
+
   it("subtract", () => {
-    const test = new TurboNumber(10);
     test.subtract(5)
     expect(test.result()).toBe(5);
   });
   it("divide", () => {
-    const test = new TurboNumber(12);
     test.divide(2);
-    expect(test.result()).toBe(6)
+    expect(test.result()).toBe(5)
   })
   it("doesn`t divide by 0", () => {
-    const test = new TurboNumber(4);
     expect( () => test.divide(0)).toThrow("Cannot divide by zero")
   })
   it("subtract and divide", () => {
